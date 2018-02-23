fff9cd82fde8110a26cf055c0d8c29dc13f05489
윤명식 <yms0059@naver.com>
2018-02-23 14:40:52 +0900

    체크박스

--- Android/MyApplication/app/src/main/java/com/example/pc/myapplication/MainActivity.java
+++ Android/MyApplication/app/src/main/java/com/example/pc/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pc.myapplication;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBarActivity;
@@ -7,33 +8,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActivityChooserView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ListView;
-
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.widget.CompoundButton.OnCheckedChangeListener;//체크박스를 위한 import
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private ArrayList<Weather>data =null;
-    private WeatherAdapter adapter=null;
-    private ListView lv=null;
+    CheckBox cb1, cb2, cb3, cb4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        data=new ArrayList<Weather>();
-        data.add(new Weather("월",R.drawable.w_icon_01,"맑음"));
-        data.add(new Weather("화", R.drawable.w_icon_02, "흐림"));
-        data.add(new Weather("수", R.drawable.w_icon_03, "흐림/비"));
-        data.add(new Weather("목", R.drawable.w_icon_04, "비"));
-        data.add(new Weather("금", R.drawable.w_icon_02, "흐림"));
-        data.add(new Weather("토", R.drawable.w_icon_01, "맑음"));
-        data.add(new Weather("일", R.drawable.w_icon_03, "흐림/비"));
+        cb1 = (CheckBox) findViewById(R.id.ch_01);
+        cb2 = (CheckBox) findViewById(R.id.ch_02);
+        cb3 = (CheckBox) findViewById(R.id.ch_03);
+        cb4 = (CheckBox) findViewById(R.id.ch_04);
 
-        adapter=new WeatherAdapter(MainActivity.this,R.layout.custom_layout,data);
-        //WeatherAdapter(사용자가 만든 어댑터)에 MainActivity의 Context와 리스트뷰를 뿌릴 cusotm_layout경로 와 data(날짜,이미지아이콘,날씨)설정
-        lv=(ListView)findViewById(R.id.lv_weather);
-        lv.setAdapter(adapter);
+        cb1.setOnCheckedChangeListener(checkedChangeListener); //아이템을 클릭(터치) 할때 생기는 이벤트
+        cb2.setOnCheckedChangeListener(checkedChangeListener);
+        cb3.setOnCheckedChangeListener(checkedChangeListener);
+        cb4.setOnCheckedChangeListener(checkedChangeListener);
     }
+
+    OnCheckedChangeListener checkedChangeListener = new OnCheckedChangeListener() {
+
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            // TODO Auto-generated method stub
+            switch (buttonView.getId()) {
+                case R.id.ch_01://들어온 값이 ch_01이라면
+                    //밑에 토스트창을 띄어준다
+                    Toast.makeText(MainActivity.this, buttonView.getText() + ", " + isChecked, Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.ch_02:
+                    Toast.makeText(MainActivity.this, buttonView.getText() + ", " + isChecked, Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.ch_03:
+                    Toast.makeText(MainActivity.this, buttonView.getText() + ", " + isChecked, Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.ch_04:
+                    Toast.makeText(MainActivity.this, buttonView.getText() + ", " + isChecked, Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
 }
+
